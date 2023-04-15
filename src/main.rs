9cce8c29a8c19ba63f22b071edcd3ae4053fbd59
Randell Crapy <randell.crapy@gmail.com>
2023-04-16 02:36:31 +0800

    My attempt of bfs style

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use core::num;
 use std::{
     thread,
     collections::{HashMap, VecDeque},
-    time::Instant, sync::mpsc::channel,
+    time::{Instant, Duration}, sync::mpsc::channel,
 };
 
 use std::sync::{
@@ -35,16 +35,17 @@ static RAND_COUNT: AtomicUsize = AtomicUsize::new(0);
 
 static OUTPUT: AtomicU64 = AtomicU64::new(0);
 
+static CUR_HEIGHT: AtomicUsize = AtomicUsize::new(0);
+
+static CUR_HEIGHT_CNT: AtomicUsize = AtomicUsize::new(0);
+static NEXT_HEIGHT_CNT: AtomicUsize = AtomicUsize::new(0);
+
 static INPUT_CNT:  AtomicUsize = AtomicUsize::new(0);
 static OUTPUT_CNT: AtomicUsize = AtomicUsize::new(0);
 
-// static ONE_HEIGHT_CNT: AtomicUsize = AtomicUsize::new(0);
-// static FINAL_CNT: AtomicUsize = AtomicUsize::new(0);
+static FINAL_H_CNT: AtomicUsize = AtomicUsize::new(0);
 
 static CPU_CNT: AtomicUsize = AtomicUsize::new(4);
-// static CHAN_SIZE: AtomicUsize = AtomicUsize::new(4);
-const Q_SIZE_MULTI: usize = 16;
-
 
 
 fn main() {
@@ -57,13 +58,18 @@ fn main() {
         "Using seed {}, starting height {}, max. children {}",
         seed, starting_height, max_children
     );
+
+    CUR_HEIGHT.store(starting_height, SeqCst);
+
     let main_cpu_cnt = CPU_CNT.load(Relaxed);
 
-    let is_done_cond = Arc::new((Mutex::new(false), Condvar::new()));
+    let is_done_cond = Arc::new((Mutex::new(0), Condvar::new()));
 
     let pool = ThreadPool::new(main_cpu_cnt);
     // CHAN_SIZE.store(main_cpu_cnt * Q_SIZE_MULTI, Relaxed);
-    let (tx, rx) = channel::unbounded::<Task>(); //channel::bounded::<Task>(CHAN_SIZE.load(Relaxed));
+    let (tx1, rx1) = channel::unbounded::<Task>(); //channel::bounded::<Task>(CHAN_SIZE.load(Relaxed));
+
+    let (tx2, rx2) = channel::unbounded::<Task>();
 
     // let mut count_map = HashMap::new(); // Dont need, split  into 3 usize variables so that tasks of different types wont wait on each other to update the count
     let mut taskq = VecDeque::from(Task::generate_initial(seed, starting_height, max_children));
@@ -71,90 +77,97 @@ fn main() {
     println!("taskq has {} tasks initially.", taskq.len());
 
     INPUT_CNT.fetch_add(taskq.len(), SeqCst);
-
-    // let mut output: u64 = 0;
-
-    // create a barrier that waits for all jobs plus the starter thread
-    // let barrier = Arc::new(Barrier::new(n_jobs + 1));
+    CUR_HEIGHT_CNT.fetch_add(taskq.len(), SeqCst);
     let start = Instant::now();
-    // while let Some(next) = taskq.pop_front() {
-    //     match next.typ {
-    //         TaskType::Derive => DERIVE_COUNT.fetch_add(1, Relaxed),
-    //         TaskType::Hash => HASH_COUNT.fetch_add(1, Relaxed),
-    //         TaskType::Random => RAND_COUNT.fetch_add(1, Relaxed),
-    //     };
-    //     // *count_map.entry(next.typ).or_insert(0usize) += 1;
-    //     let result = next.execute();
-    //     println!("Result length: {}",result.1.len());
-    //     if result.1.len() > 0 {
-    //         println!("New height: {}",result.1[0].height);
-    //     }
-        
-    //     // output ^= result.0;
-    //     OUTPUT.fetch_xor(result.0, Relaxed);
-    //     if result.1.len() == 0 {
-    //         FINAL_CNT.fetch_add(1, Relaxed);
-    //     }
-    //     taskq.extend(result.1.into_iter());
-    // }
-
     while let Some(init_next) = taskq.pop_front() {
+        match starting_height % 2 {
+            0 => tx1.send(init_next.clone()).unwrap(),
+            1 => tx2.send(init_next.clone()).unwrap(),
+            _ => todo!()
+        }
+    }
+
+    for _ in 0..main_cpu_cnt{
         let is_done_cond2 = Arc::clone(&is_done_cond);
-        let t_tx = tx.clone();
-        let t_rx = rx.clone();
+        let (t_tx1, t_rx1) = (tx1.clone(), rx1.clone());
+        let (t_tx2, t_rx2) = (tx2.clone(), rx2.clone());
         pool.execute(move || {
-            // let mut final_cnt = FINAL_CNT.load(Relaxed);
-            // let mut one_cnt: usize = ONE_HEIGHT_CNT.load(Relaxed);
-            // one_cnt == 0 || final_cnt < one_cnt
-            while  INPUT_CNT.load(SeqCst) > OUTPUT_CNT.load(SeqCst) {
-                let next = t_rx.recv().unwrap();
-                println!("Thread: Received task at height {}", next.height);
-                match next.typ {
-                    TaskType::Derive => DERIVE_COUNT.fetch_add(1, Relaxed),
-                    TaskType::Hash => HASH_COUNT.fetch_add(1, Relaxed),
-                    TaskType::Random => RAND_COUNT.fetch_add(1, Relaxed),
+            while INPUT_CNT.load(SeqCst) > OUTPUT_CNT.load(SeqCst) {
+                let cur_height = CUR_HEIGHT.load(SeqCst);
+                // Consume all current height tasks
+                println!("Thread: Consume all current height tasks {}", cur_height);
+                let (cur_tx, cur_rx) = match cur_height % 2 {
+                    0 => (&t_tx2, &t_rx1),
+                    1 => (&t_tx1, &t_rx2),
+                    _ => todo!()
                 };
-                let result = next.execute();
-                OUTPUT.fetch_xor(result.0, Relaxed);
-                INPUT_CNT.fetch_add(result.1.len(), SeqCst);
-                OUTPUT_CNT.fetch_add(1, SeqCst);
-                // if result.1.len() > 0 && result.1[0].height == 1 {
-                //     // println!("Thread: I have reached height 1!");
-                //     ONE_HEIGHT_CNT.fetch_add(result.1.len(), SeqCst);
-                // }
-                // if next.height == 0 {
-                //     println!("Thread: I have reached height 0!");
-                //     FINAL_CNT.fetch_add(1, SeqCst);
-                // }
-                // println!("Pushing {} new tasks ", result.1.len());
-                for new_task in result.1.iter() {
-                    // t_tx.se
-                    t_tx.send(new_task.clone()).unwrap();
+                while !cur_rx.is_empty(){
+                    let next = cur_rx.recv().unwrap();
+                    // println!("Thread: Received task at height {}", next.height);
+                    match next.typ {
+                        TaskType::Derive => DERIVE_COUNT.fetch_add(1, Relaxed),
+                        TaskType::Hash => HASH_COUNT.fetch_add(1, Relaxed),
+                        TaskType::Random => RAND_COUNT.fetch_add(1, Relaxed),
+                    };
+                    let result = next.execute();
+                    OUTPUT.fetch_xor(result.0, Relaxed);
+                    INPUT_CNT.fetch_add(result.1.len(), SeqCst);
+                    NEXT_HEIGHT_CNT.fetch_add(result.1.len(), SeqCst);
+                    OUTPUT_CNT.fetch_add(1, SeqCst);
+
+                    // Fill with all next height tasks
+                    for new_task in result.1.iter() {
+                        cur_tx.send(new_task.clone()).unwrap();
+                    }
+                    CUR_HEIGHT_CNT.fetch_sub(1, SeqCst);
                 }
-                // one_cnt = ONE_HEIGHT_CNT.load(Relaxed);
-                // final_cnt = FINAL_CNT.load(Relaxed);
-                // println!("one_cnt: {one_cnt}");
-                // println!("final_cnt: {final_cnt}");
-                println!("Input vs out: {}, {}", INPUT_CNT.load(SeqCst), OUTPUT_CNT.load(SeqCst));
+                while CUR_HEIGHT_CNT.load(SeqCst) > 0 {}
+                // println!("Finished all on this level");
+                if cur_height == 0 {
+                    println!("Finished all levels!");
+                    println!("Unlocking the exit");
+                    let (lock, cvar) = &*is_done_cond2;
+                    let mut is_done = lock.lock().unwrap();
+                    *is_done += 1; // true
+                    cvar.notify_one();
+                    FINAL_H_CNT.fetch_add(1, SeqCst);
+                    return;
+                    // break;
+                }
+                match CUR_HEIGHT.compare_exchange(cur_height, cur_height - 1, SeqCst, SeqCst){
+                    Ok(_) => {}
+                    Err(_) => {}
+                };
+                let next_height = NEXT_HEIGHT_CNT.load(SeqCst);
+                match CUR_HEIGHT_CNT.compare_exchange(0, NEXT_HEIGHT_CNT.load(SeqCst), SeqCst, SeqCst){
+                    Ok(_) => {},
+                    Err(_) => {}
+                };
+
+                match NEXT_HEIGHT_CNT.compare_exchange(next_height, 0, SeqCst, SeqCst){
+                    Ok(_) => {},
+                    Err(_) => {}
+                };
+
+                // println!("Input vs out: {}, {}", INPUT_CNT.load(SeqCst), OUTPUT_CNT.load(SeqCst));
+                // println!("NEXT_HEIGHT_CNT vs CUR_HEIGHT_CNT: {}, {}", NEXT_HEIGHT_CNT.load(SeqCst), CUR_HEIGHT_CNT.load(SeqCst));
             }
-
-            let (lock, cvar) = &*is_done_cond2;
-            let mut is_done = lock.lock().unwrap();
-            *is_done = true;
-            cvar.notify_one();
         });
-        tx.send(init_next.clone()).unwrap();
     }
 
-    // while FINAL_CNT.load(Relaxed) < CPU_CNT.load(Relaxed) {
+    // let (lock, cvar) = &*is_done_cond;
+    // let mut is_done = lock.lock().unwrap();
 
+    
+    // while *is_done < main_cpu_cnt {  
+    //     is_done = cvar.wait(is_done).unwrap();
+    //     println!("Isdone is now {is_done}");
     // }
-    let (lock, cvar) = &*is_done_cond;
-    let mut is_done = lock.lock().unwrap();
 
-    while !*is_done {
-        is_done = cvar.wait(is_done).unwrap();
-    }
+    while FINAL_H_CNT.load(SeqCst) < main_cpu_cnt {
+        println!("FINAL_H_CNT: {}", FINAL_H_CNT.load(SeqCst));
+        thread::sleep(Duration::from_secs(2));
+    };
     
     println!("FINAL Input vs out: {}, {}", INPUT_CNT.load(SeqCst), OUTPUT_CNT.load(SeqCst));
 
